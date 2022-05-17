387df4c93928fcd61c7c2594285a17931c1a3392
Maninaid <103981237+Maninaid@users.noreply.github.com>
2022-05-17 18:34:17 +0530

    Corebasedexam2

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corebasedexam2
+{
+        public delegate void calculator(int a, int b);
+
+
+
+    class Program
+    {
+        public static void addition(int a, int b)
+        {
+            int res = a + b;
+            Console.WriteLine($"Addition result is {res}");
+        }
+        public static void substraction(int a, int b)
+        {
+            int res = a - b;
+            Console.WriteLine($"substraction result is {res}");
+        }
+        public static void multiplication(int a, int b)
+        {
+            int res = a * b;
+            Console.WriteLine($" multiplication result is {res}");
+        }
+        public static void division(int a, int b)
+        {
+            int res = a / b;
+            Console.WriteLine($" division result is {res}");
+        }
+        static void Main(string[] args)
+        {
+            calculator obj = new calculator(Program.addition);
+            obj.Invoke(13, 20);
+            obj = substraction;
+            obj(32, 10);
+            obj = multiplication;
+            obj(20, 40);
+            obj = division;
+            obj(60, 10);
+            Console.ReadLine();
+        }
+    }
+}
